67610f5bace7390ee09b4564cc219d8ac6e6bd62
榊原昌彦 <sakakibara@rdlabo.jp>
2019-06-14 16:22:39 +0900

    初期表示のModuleを削除

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckTutorial } from './providers/check-tutorial.service';
+import {TabsPage} from './pages/tabs-page/tabs-page';
+import {SchedulePage} from './pages/schedule/schedule';
 
 const routes: Routes = [
   {
@@ -26,7 +28,67 @@ const routes: Routes = [
   },
   {
     path: 'app',
-    loadChildren: './pages/tabs-page/tabs-page.module#TabsModule'
+    children: [
+      {
+        path: 'tabs',
+        component: TabsPage,
+        children: [
+          {
+            path: 'schedule',
+            children: [
+              {
+                path: '',
+                component: SchedulePage,
+              },
+              {
+                path: 'session/:sessionId',
+                loadChildren: '../session-detail/session-detail.module#SessionDetailModule'
+              }
+            ]
+          },
+          {
+            path: 'speakers',
+            children: [
+              {
+                path: '',
+                loadChildren: '../speaker-list/speaker-list.module#SpeakerListModule'
+              },
+              {
+                path: 'session/:sessionId',
+                loadChildren: '../session-detail/session-detail.module#SessionDetailModule'
+              },
+              {
+                path: 'speaker-details/:speakerId',
+                loadChildren: '../speaker-detail/speaker-detail.module#SpeakerDetailModule'
+              }
+            ]
+          },
+          {
+            path: 'map',
+            children: [
+              {
+                path: '',
+                loadChildren: '../map/map.module#MapModule'
+              }
+            ]
+          },
+          {
+            path: 'about',
+            children: [
+              {
+                path: '',
+                loadChildren: '../about/about.module#AboutModule'
+              }
+            ]
+          },
+          {
+            path: '',
+            redirectTo: '/app/tabs/schedule',
+            pathMatch: 'full'
+          }
+        ]
+      }
+    ],
   },
   {
     path: 'tutorial',
